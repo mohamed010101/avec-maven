27cc0d7d2aa38fb4443b8b837a9a1b0a3a013a1c
Ben Youssef Mohamed <mohamed.benyoussef@esprit.tn>
2023-01-18 11:20:13 +0100

    Third commit-V-03

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -19,6 +19,7 @@ import org.json.JSONObject;
  *
  */
 /* Mohamed est ici*/
+	/*branch mohamed*/
 public class CallRestWebService {
 
 	public static final String endpoint = "http://ip-api.com/json";
